ae75da2f36cb60aeeb6b02788d84d2502db0c931
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-04-12 21:30:04 +0700

    Removed unnecessary modes, added B&W mode

--- app/src/main/java/org/beyang/beras/MainActivity.java
+++ app/src/main/java/org/beyang/beras/MainActivity.java
@@ -32,22 +32,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private static final String TAG = "OCVSample::Activity";
 
     public static final int VIEW_MODE_RGBA = 0;
-    public static final int VIEW_MODE_HIST = 1;
-    public static final int VIEW_MODE_CANNY = 2;
-    public static final int VIEW_MODE_SEPIA = 3;
-    public static final int VIEW_MODE_SOBEL = 4;
-    public static final int VIEW_MODE_ZOOM = 5;
-    public static final int VIEW_MODE_PIXELIZE = 6;
-    public static final int VIEW_MODE_POSTERIZE = 7;
+    public static final int VIEW_MODE_CANNY = 1;
+    public static final int VIEW_MODE_BW = 2;
 
     private MenuItem mItemPreviewRGBA;
-    private MenuItem mItemPreviewHist;
     private MenuItem mItemPreviewCanny;
-    private MenuItem mItemPreviewSepia;
-    private MenuItem mItemPreviewSobel;
-    private MenuItem mItemPreviewZoom;
-    private MenuItem mItemPreviewPixelize;
-    private MenuItem mItemPreviewPosterize;
+    private MenuItem mItemPreviewBW;
     private CameraBridgeViewBase mOpenCvCameraView;
 
     private Size mSize0;
@@ -139,14 +129,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         Log.i(TAG, "called onCreateOptionsMenu");
-        mItemPreviewRGBA = menu.add("Preview RGBA");
-        mItemPreviewHist = menu.add("Histograms");
+        mItemPreviewRGBA = menu.add("Normal");
         mItemPreviewCanny = menu.add("Canny");
-        mItemPreviewSepia = menu.add("Sepia");
-        mItemPreviewSobel = menu.add("Sobel");
-        mItemPreviewZoom = menu.add("Zoom");
-        mItemPreviewPixelize = menu.add("Pixelize");
-        mItemPreviewPosterize = menu.add("Posterize");
+        mItemPreviewBW = menu.add("Black&White");
         return true;
     }
 
@@ -155,20 +140,10 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         Log.i(TAG, "called onOptionsItemSelected; selected item: " + item);
         if (item == mItemPreviewRGBA)
             viewMode = VIEW_MODE_RGBA;
-        if (item == mItemPreviewHist)
-            viewMode = VIEW_MODE_HIST;
         else if (item == mItemPreviewCanny)
             viewMode = VIEW_MODE_CANNY;
-        else if (item == mItemPreviewSepia)
-            viewMode = VIEW_MODE_SEPIA;
-        else if (item == mItemPreviewSobel)
-            viewMode = VIEW_MODE_SOBEL;
-        else if (item == mItemPreviewZoom)
-            viewMode = VIEW_MODE_ZOOM;
-        else if (item == mItemPreviewPixelize)
-            viewMode = VIEW_MODE_PIXELIZE;
-        else if (item == mItemPreviewPosterize)
-            viewMode = VIEW_MODE_POSTERIZE;
+        else if (item == mItemPreviewBW)
+            viewMode = VIEW_MODE_BW;
         return true;
     }
 
@@ -227,47 +202,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             case MainActivity.VIEW_MODE_RGBA:
                 break;
 
-            case MainActivity.VIEW_MODE_HIST:
-                Mat hist = new Mat();
-                int thikness = (int) (sizeRgba.width / (mHistSizeNum + 10) / 5);
-                if (thikness > 5) thikness = 5;
-                int offset = (int) ((sizeRgba.width - (5 * mHistSizeNum + 4 * 10) * thikness) / 2);
-                // RGB
-                for (int c = 0; c < 3; c++) {
-                    Imgproc.calcHist(Arrays.asList(rgba), mChannels[c], mMat0, hist, mHistSize, mRanges);
-                    Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
-                    hist.get(0, 0, mBuff);
-                    for (int h = 0; h < mHistSizeNum; h++) {
-                        mP1.x = mP2.x = offset + (c * (mHistSizeNum + 10) + h) * thikness;
-                        mP1.y = sizeRgba.height - 1;
-                        mP2.y = mP1.y - 2 - (int) mBuff[h];
-                        Imgproc.line(rgba, mP1, mP2, mColorsRGB[c], thikness);
-                    }
-                }
-                // Value and Hue
-                Imgproc.cvtColor(rgba, mIntermediateMat, Imgproc.COLOR_RGB2HSV_FULL);
-                // Value
-                Imgproc.calcHist(Arrays.asList(mIntermediateMat), mChannels[2], mMat0, hist, mHistSize, mRanges);
-                Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
-                hist.get(0, 0, mBuff);
-                for (int h = 0; h < mHistSizeNum; h++) {
-                    mP1.x = mP2.x = offset + (3 * (mHistSizeNum + 10) + h) * thikness;
-                    mP1.y = sizeRgba.height - 1;
-                    mP2.y = mP1.y - 2 - (int) mBuff[h];
-                    Imgproc.line(rgba, mP1, mP2, mWhilte, thikness);
-                }
-                // Hue
-                Imgproc.calcHist(Arrays.asList(mIntermediateMat), mChannels[0], mMat0, hist, mHistSize, mRanges);
-                Core.normalize(hist, hist, sizeRgba.height / 2, 0, Core.NORM_INF);
-                hist.get(0, 0, mBuff);
-                for (int h = 0; h < mHistSizeNum; h++) {
-                    mP1.x = mP2.x = offset + (4 * (mHistSizeNum + 10) + h) * thikness;
-                    mP1.y = sizeRgba.height - 1;
-                    mP2.y = mP1.y - 2 - (int) mBuff[h];
-                    Imgproc.line(rgba, mP1, mP2, mColorsHue[h], thikness);
-                }
-                break;
-
             case MainActivity.VIEW_MODE_CANNY:
                 rgbaInnerWindow = rgba.submat(top, top + height, left, left + width);
                 Imgproc.Canny(rgbaInnerWindow, mIntermediateMat, 80, 90);
@@ -275,46 +209,9 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                 rgbaInnerWindow.release();
                 break;
 
-            case MainActivity.VIEW_MODE_SOBEL:
-                Mat gray = inputFrame.gray();
-                Mat grayInnerWindow = gray.submat(top, top + height, left, left + width);
+            case MainActivity.VIEW_MODE_BW:
                 rgbaInnerWindow = rgba.submat(top, top + height, left, left + width);
-                Imgproc.Sobel(grayInnerWindow, mIntermediateMat, CvType.CV_8U, 1, 1);
-                Core.convertScaleAbs(mIntermediateMat, mIntermediateMat, 10, 0);
-                Imgproc.cvtColor(mIntermediateMat, rgbaInnerWindow, Imgproc.COLOR_GRAY2BGRA, 4);
-                grayInnerWindow.release();
-                rgbaInnerWindow.release();
-                break;
-
-            case MainActivity.VIEW_MODE_SEPIA:
-                rgbaInnerWindow = rgba.submat(top, top + height, left, left + width);
-                Core.transform(rgbaInnerWindow, rgbaInnerWindow, mSepiaKernel);
-                rgbaInnerWindow.release();
-                break;
-
-            case MainActivity.VIEW_MODE_ZOOM:
-                Mat zoomCorner = rgba.submat(0, rows / 2 - rows / 10, 0, cols / 2 - cols / 10);
-                Mat mZoomWindow = rgba.submat(rows / 2 - 9 * rows / 100, rows / 2 + 9 * rows / 100, cols / 2 - 9 * cols / 100, cols / 2 + 9 * cols / 100);
-                Imgproc.resize(mZoomWindow, zoomCorner, zoomCorner.size(), 0, 0, Imgproc.INTER_LINEAR_EXACT);
-                Size wsize = mZoomWindow.size();
-                Imgproc.rectangle(mZoomWindow, new Point(1, 1), new Point(wsize.width - 2, wsize.height - 2), new Scalar(255, 0, 0, 255), 2);
-                zoomCorner.release();
-                mZoomWindow.release();
-                break;
-
-            case MainActivity.VIEW_MODE_PIXELIZE:
-                rgbaInnerWindow = rgba.submat(top, top + height, left, left + width);
-                Imgproc.resize(rgbaInnerWindow, mIntermediateMat, mSize0, 0.1, 0.1, Imgproc.INTER_NEAREST);
-                Imgproc.resize(mIntermediateMat, rgbaInnerWindow, rgbaInnerWindow.size(), 0., 0., Imgproc.INTER_NEAREST);
-                rgbaInnerWindow.release();
-                break;
-
-            case MainActivity.VIEW_MODE_POSTERIZE:
-                rgbaInnerWindow = rgba.submat(top, top + height, left, left + width);
-                Imgproc.Canny(rgbaInnerWindow, mIntermediateMat, 80, 90);
-                rgbaInnerWindow.setTo(new Scalar(0, 0, 0, 255), mIntermediateMat);
-                Core.convertScaleAbs(rgbaInnerWindow, mIntermediateMat, 1. / 16, 0);
-                Core.convertScaleAbs(mIntermediateMat, rgbaInnerWindow, 16, 0);
+                Imgproc.cvtColor(mIntermediateMat, rgbaInnerWindow, Imgproc.COLOR_RGB2GRAY);
                 rgbaInnerWindow.release();
                 break;
         }
