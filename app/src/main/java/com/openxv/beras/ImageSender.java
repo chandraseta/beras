82ed7a8c03d2300a06c91eba635919f27fe59aad
Felix Limanta <13515065@std.stei.itb.ac.id>
2018-05-05 22:05:58 +0700

    Connected app to server

--- app/src/main/java/com/openxv/beras/ImageSender.java
+++ app/src/main/java/com/openxv/beras/ImageSender.java
@@ -8,44 +8,26 @@ import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.DataOutput;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ImageSender extends AsyncTask<String, Void, String> {
+    private final static String API_URL = "https://beras-server.herokuapp.com/predict";
+    private final static String MSG_SUCCESS = "Tipe Beras: ";
+    private final static String MSG_FAILURE = "Gagal menentukan tipe beras";
+    private final static String MSG_ERROR = "Terjadi kesalahan pada server";
+    private final static String boundary = "*****";
+    private final static String crlf = "\r\n";
+    private final static String twoHyphens = "--";
     private TextView resultText;
-    private static String API_URL = "";
-    private static String boundary = "*****";
-    private static String crlf = "\r\n";
-    private static String twoHyphens = "--";
 
     public ImageSender(TextView resultText) {
         this.resultText = resultText;
-    }
-
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-
-        try {
-            JSONObject jsonResult = new JSONObject(s);
-            String kelas = jsonResult.getString("class");
-            resultText.setText("Tipe Beras : "+ kelas);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    protected String doInBackground(String... strings) {
-        return sendImage(strings[0]);
     }
 
     private static String sendImage(String path) {
@@ -62,7 +44,7 @@ public class ImageSender extends AsyncTask<String, Void, String> {
             File file = new File(path);
             String fileName = file.getName();
 
-            byte[] bytes = new byte[(int)file.length()];
+            byte[] bytes = new byte[(int) file.length()];
             BufferedInputStream buf = new BufferedInputStream(new FileInputStream(file));
             buf.read(bytes, 0, bytes.length);
             buf.close();
@@ -82,32 +64,54 @@ public class ImageSender extends AsyncTask<String, Void, String> {
             request.close();
 
             response = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuffer buffer = new StringBuffer();
+            StringBuilder buffer = new StringBuilder();
             String line;
             while ((line = response.readLine()) != null) {
-                buffer.append(line + "\n");
+                buffer.append(line).append("\n");
             }
 
-            if(buffer.length() == 0){
+            if (buffer.length() == 0) {
                 return null;
             }
             result = buffer.toString();
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
-            if(connection != null){
+            if (connection != null) {
                 connection.disconnect();
             }
-            if(response != null){
+            if (response != null) {
                 try {
                     response.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-
-            return result;
         }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        try {
+            JSONObject jsonResult = new JSONObject(s);
+            if (jsonResult.getBoolean("success")) {
+                String kelas = jsonResult.getString("class");
+                resultText.setText(String.format("%s%s", MSG_SUCCESS, kelas));
+            } else {
+                resultText.setText(MSG_FAILURE);
+            }
+        } catch (JSONException e) {
+            resultText.setText(MSG_ERROR);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return sendImage(strings[0]);
     }
 
 }
